355ca51f58155b2c5ad27d6612e59e6ca35f5017
René Emmaneel <reneemmaneel00@gmail.com>
2023-12-01 16:47:01 +0100

    Added digits function to utils

--- utils.py
+++ utils.py
@@ -8,5 +8,8 @@ def ints(s: str) -> typing.List[int]:
     assert isinstance(s, str), f"you passed in a {type(s)}!!!"
     return lmap(int, re.findall(r"(?:(?<!\d)-)?\d+", s))  # thanks mserrano!
 
+def digits(s: str) -> typing.List[int]:
+    return [int(c) for c in s if c.isdigit()]
+
 def strs(s: str) -> typing.List[str]:
     return s.strip().split(' ')
